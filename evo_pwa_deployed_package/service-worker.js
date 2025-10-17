260e7bb8451c07123bfa5236de7d8831ade46e06
capria1981-cmyk <capria1981@gmail.com>
2025-10-17 11:00:48 -0400

    Add files via upload

---
+++ evo_pwa_deployed_package/service-worker.js
@@ -0,0 +1,21 @@
+const CACHE_NAME = 'evo-cache-v1';
+const FILES_TO_CACHE = [
+  '/index.html',
+  '/manifest.json',
+  '/icons/icon-192.png',
+  '/icons/icon-512.png'
+];
+self.addEventListener('install', (evt) => {
+  evt.waitUntil(caches.open(CACHE_NAME).then((cache) => cache.addAll(FILES_TO_CACHE)));
+  self.skipWaiting();
+});
+self.addEventListener('activate', (evt) => {
+  evt.waitUntil(self.clients.claim());
+});
+self.addEventListener('fetch', (evt) => {
+  if (evt.request.mode === 'navigate') {
+    evt.respondWith(fetch(evt.request).catch(() => caches.match('/index.html')));
+    return;
+  }
+  evt.respondWith(caches.match(evt.request).then((resp) => resp || fetch(evt.request)));
+});
